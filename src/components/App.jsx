a2748da607f0d38dd160e812b14d9fd5577514cd
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-09-22 18:16:36 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { Section } from './Section/Section';
 import { ButtonList } from './Buttons/Buttons';
 import { Feedback } from './FeedbackList/FeedbackList';
@@ -10,6 +10,7 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
   data = {
     isClicked: false,
   };
@@ -27,7 +28,7 @@ export class App extends Component {
   render() {
     return (
       <>
-        <Section title="Please leave feedback" priority={1}>
+        <Section title="Please leave feedback  -> Class" priority={1}>
           <ButtonList
             options={['good', 'neutral', 'bad']}
             onLeaveFeedback={this.onLeaveFeedback}
@@ -53,3 +54,47 @@ export class App extends Component {
     );
   }
 }
+
+export const App1 = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onLeaveFeedback = type => {
+    setFeedback({ ...feedback, [type]: feedback[type] + 1 });
+    setIsClicked(true);
+  };
+  const countTotalFeedback = () => {
+    const total = Object.values(feedback).reduce((acc, el) => acc + el, 0);
+    return total;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((feedback.good / countTotalFeedback()) * 100);
+  };
+
+  return (
+    <>
+      <Section title="Please leave feedback -> Hook" priority={1}>
+        <ButtonList
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics" priority={2}>
+        {isClicked ? (
+          <Feedback
+            list={[
+              `good: ${feedback.good}`,
+              `neutral: ${feedback.neutral}`,
+              `bad: ${feedback.bad}`,
+              `Total Feedback: ${countTotalFeedback()}`,
+              `Positive Feedback: ${countPositiveFeedbackPercentage()}%`,
+            ]}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
+};
